e72ba212ec91c9661628eb81b4739e0c8319e03f
Max Burke <maxburke@gmail.com>
2012-01-05 16:44:28 -0800

    Modified the post schema slightly so that we store bytes instead of uuids because this makes the code simpler as there's no need to convert to PG's UUID code, we just stuff bytes into the database. Done and done.

--- kobbweb/server/create-schema.lisp
+++ kobbweb/server/create-schema.lisp
@@ -15,7 +15,7 @@
    (:create-table posts
     ((post_id :type serial :primary-key t)
      (user_id :type integer)
-     (content_id :type 'uuid))
+     (item_id :type bytea))
    )
 ;   (:create-table locations
 ;    ((location_id :type serial :primary-key t)
