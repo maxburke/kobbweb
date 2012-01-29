ebce2dea09a5adf98a553db80571884020271f24
Max Burke <maxburke@gmail.com>
2012-01-28 20:51:55 -0800

    Removed a bunch of code from the schema creation file that was not related to this project.

--- kobbweb/server/create-schema.lisp
+++ kobbweb/server/create-schema.lisp
@@ -25,27 +25,6 @@
      (alias :type string))
    )
   )
-;   (:create-table locations
-;    ((location_id :type serial :primary-key t)
-;     (name :type text)
-;     (latitude :type double-float)
-;     (longitude :type double-float))
-;   )
-;   (:create-table friends
-;    ((user_id :type integer :references (users :no-action :no-action))
-;     (friend_id :type integer :references (users :no-action :no-action))
-;     (relationship :type integer :default 0))
-;   )
-;   (:create-table dive_log_entry
-;    ((dive_log_id :type serial :primary-key t)
-;     (user_id :type integer :references (users :no-action :no-action))
-;     (location_id :type integer :references (locations :no-action :no-action))
-;     (date :type timestamp)
-;     (duration :type integer)
-;     (depth :type integer)
-;     (partner :type integer :references (users :no-action :no-action))
-;     (notes :type text))
-;   )
  )
 )
 
